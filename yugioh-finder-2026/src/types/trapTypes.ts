7910c22b4a8c2f9674e8f500bd01571abdf093fd
UnluckySupp <leo.vigliocco96@gmail.com>
2026-01-06 16:33:24 -0300

    feature: define trap types and payload structures for enhanced card functionality

--- yugioh-finder-2026/src/types/trapTypes.ts
+++ yugioh-finder-2026/src/types/trapTypes.ts
@@ -1,5 +1,33 @@
+import {
+  type GenericCardPayload,
+  type CardImagesPayload,
+  type CardPricesPayload,
+  type CardSetPayload,
+} from "./genericTypes";
+
 // Trap Types
-export type TrapType = 
+export type TrapTypePayload = 
   | "Normal Trap"
   | "Continuous Trap"
   | "Counter Trap"
+  | " Trap";
+
+export type TrapRacePayload = 
+  | "Normal"
+  | "Continuous"
+  | "Counter"
+  | "";
+
+export type TrapFramePayload = "trap";
+
+export type GenericTrapPayload = GenericCardPayload & {
+  type: TrapTypePayload;
+  race: TrapRacePayload;
+  frameType: TrapFramePayload;
+  humanReadableCardType: TrapTypePayload;
+  card_images: CardImagesPayload[];
+  card_prices: CardPricesPayload[];
+  card_sets: CardSetPayload[];
+};
+
+export type TrapCardPayload = GenericTrapPayload;
