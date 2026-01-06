62d22ac984be227910bc3b38e075e40cd4e585f6
UnluckySupp <leo.vigliocco96@gmail.com>
2026-01-06 16:33:13 -0300

    feature: expand spell types and payload structures for enhanced card functionality

--- yugioh-finder-2026/src/types/spellTypes.ts
+++ yugioh-finder-2026/src/types/spellTypes.ts
@@ -1,8 +1,37 @@
+import {
+  type GenericCardPayload,
+  type CardImagesPayload,
+  type CardPricesPayload,
+  type CardSetPayload,
+} from "./genericTypes";
+
 // Spell Types
-export type SpellType = 
+export type SpellTypePayload = 
   | "Continuous Spell"
   | "Quick-Play Spell"
   | "Equip Spell"
   | "Normal Spell"
   | "Field Spell"
   | "Ritual Spell";
+
+export type SpellRacePayload = 
+  | "Continuous"
+  | "Quick-Play"
+  | "Equip"
+  | "Normal"
+  | "Field"
+  | "Ritual";
+
+export type SpellFramePayload = "spell";
+
+export type GenericSpellPayload = GenericCardPayload & {
+  type: SpellTypePayload;
+  race: SpellRacePayload;
+  frameType: SpellFramePayload;
+  humanReadableCardType: SpellTypePayload;
+  card_images: CardImagesPayload[];
+  card_prices: CardPricesPayload[];
+  card_sets: CardSetPayload[];
+};
+
+export type SpellCardPayload = GenericSpellPayload;
